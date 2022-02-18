782c0e73c8db58ee11a5bc888391c469b314871c
DESKTOP-K1PPJ6R\Admin <dpoluektov@gmail.com>
2022-02-18 23:13:18 +0200

    Controller: Get Commands for Platform

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    // Example: GET http://localhost:6000/api/c/platforms/1/commands
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommandRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
+        {
+            Console.WriteLine($"--> Hit GetCommandsForPlatform: {platformId}");
+
+            if(!_repository.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+            
+            var commands = _repository.GetCommandsForPlatform(platformId);
+
+            return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+    }
+}
