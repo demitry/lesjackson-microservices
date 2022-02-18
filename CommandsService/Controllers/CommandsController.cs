9ee92e93ec5d36b91c71eaa73016a2323fa3b6f2
DESKTOP-K1PPJ6R\Admin <dpoluektov@gmail.com>
2022-02-18 23:43:20 +0200

    Controller: Create Command For Platform

--- CommandsService/Controllers/CommandsController.cs
+++ CommandsService/Controllers/CommandsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using AutoMapper;
 using CommandsService.Data;
 using CommandsService.Dtos;
+using CommandsService.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -55,6 +56,34 @@ namespace CommandsService.Controllers
             }
 
             return Ok(_mapper.Map<CommandReadDto>(command));
+        }
+
+        //Example: 
+        // POST http://localhost:6000/api/c/platforms/1/commands
+        //Body:
+        // {
+        // 	"HowTo" : "Build a dotnet Project",
+        // 	"CommandLine" : "dotnet build"
+        // }
+        [HttpPost]
+        public ActionResult<CommandReadDto> CreateCommandForPlatform(int platformId, CommandCreateDto commandDto)
+        {
+            Console.WriteLine($"--> Hit CreateCommandForPlatform: {platformId}");
+
+            if(!_repository.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+
+            var command = _mapper.Map<Command>(commandDto);
+
+            _repository.CreateCommand(platformId, command);
+            _repository.SaveChanges();
+
+            var commandReadDto = _mapper.Map<CommandReadDto>(command);
+
+            return CreatedAtRoute(nameof(GetCommandForPlatform),
+                new {platformId = platformId, commandId = commandReadDto.Id}, commandReadDto);
         }
     }
 }
